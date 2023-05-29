49707f825d6e5f6e635d17c01bc46dc3a3a943ca
LittlemilkWu <leowu0819@gmail.com>
2023-05-29 15:19:11 +0800

    feat: add ArgumentParser

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from AutoMerchandiseTrack import AutoMerchandiseTrack
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument('--time_interval', '-t', default=900, type=int)
+parser.add_argument('--quote', '-q', type=str)
+parser.add_argument('--board', '-b', default='MacShop', type=str)
+parser.add_argument('--end_time', '-e', default='2021-09-20T00:00:00', type=str)
+args = parser.parse_args()
+
+TIME_INTERVAL = args.time_interval
+QUOTE = args.quote
+BOARD = args.board
+END_TIME = args.end_time
 
 if __name__ == "__main__":
     ''' params: time_interval, merchandise, board, stop_time
     '''
-    AMT = AutoMerchandiseTrack(900, 'iphone 8', 'MacShop', '2021-09-20T00:00:00');
-    AMT.start_tracking();
+    AMT = AutoMerchandiseTrack(TIME_INTERVAL, QUOTE, BOARD, END_TIME)
+    AMT.start_tracking()
